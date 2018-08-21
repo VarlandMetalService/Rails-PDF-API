c59f67365cef3c2d13cf58036795fc34a6346c7e
dom-VMS <dom.aracri@varland.com>
2018-08-21 16:01:16 -0400

    [Shop Order] Added JSON integration
    Continued updates, added JSON, nearly complete. Data is now being used from JSON file.

--- app/pdfs/so.rb
+++ app/pdfs/so.rb
@@ -15,8 +15,16 @@ class SO < VarlandPdf
     @data = JSON.parse(file)
 
     @color = color
+    case @data['schedule_code']
+    when 'YEL'
+      @color = 'yellow'
+    when 'GRN'
+      @color = 'green'
+    else
+      @color = 'blue'
+    end
 
-    print_text
+    print_instructions
     print_header
     first_page_header
   end
@@ -29,21 +37,51 @@ class SO < VarlandPdf
     # Draw revision dates.
     bounding_box [_i(0.25), _i(3.45)], width: _i(8), height: _i(0.8) do
       page_header_data_box 'REV DATES:', -0.05, 0.8, 1.5, 0.2, :left
+
       headings = ['SPEC INST', 'STD PROC', 'LOADINGS', 'PART SPEC']
-      dates = ['12/11/15', '04/06/18', '12/23/15', '04/06/18'] # START HERE ON FRIDAY
-      times = ['16:22', '13:06', '12:07', '13:06']
-      initials = ['JP', 'MMJ', 'BM', 'MMJ']
+
+      dates = [
+        @data['revision_dates']['special_instructions']['timestamp*'], 
+        @data['revision_dates']['standard_procedure']['timestamp*'], 
+        @data['revision_dates']['loadings']['timestamp*'], 
+        @data['revision_dates']['part_spec']['timestamp*'] 
+      ]
+
+      #Formats date/time
+      for i in (0..dates.length)
+        if dates[i] != nil
+          dates[i] = dates[i][0...-6]
+        end
+      end
+
+      initials = [
+        [@data['revision_dates']['special_instructions']['operator_initials'],@data['revision_dates']['special_instructions']['operator_initials']], 
+        [@data['revision_dates']['standard_procedure']['operator_initials'], @data['revision_dates']['standard_procedure']['operator_initials']], 
+        [@data['revision_dates']['loadings']['operator_initials'], @data['revision_dates']['loadings']['operator_initials']], 
+        [@data['revision_dates']['part_spec']['operator_initials'], @data['revision_dates']['part_spec']['operator_initials']]
+      ]
+
       y = 0.8
       0.upto(3) do |i|
         page_header_data_box headings[i], 1, y, 1.5, 0.2, :left
         page_header_data_box dates[i], 1.8, y, 1.5, 0.2, :left
-        page_header_data_box "#{initials[i]}:#{initials[i]}", 2.4, y, 1.5, 0.2, :left
-        page_header_data_box times[i], 3.1, y, 1.5, 0.2, :left
+        if(initials[i][0] != nil|| initials[i][1] != nil) #Formats initials
+          page_header_data_box "#{initials[i][0]}:#{initials[i][1]}", 2.4, y, 1.5, 0.2, :right
+        end
+        #page_header_data_box times[i], 3.1, y, 1.5, 0.2, :left
         y -= 0.2
       end
-      headings = ['LAST ORDER', 'PREV ORDER', 'QUOTE ART']
-      dates = ['04/03/18', '06/26/17', '01/29/18']
-      numbers = ['284439', '277270', '69419']
+      headings = ['LAST ORDER', 'PREV ORDER', 'QUOTE INFO']
+      dates = [
+        @data['revision_dates']['last_order']['date*'], 
+        @data['revision_dates']['previous_order']['date*'], 
+        @data['revision_dates']['quote']['date*']
+      ]
+      numbers = [
+        @data['revision_dates']['last_order']['number'].to_s, 
+        @data['revision_dates']['previous_order']['number'].to_s, 
+        @data['revision_dates']['quote']['number'].to_s
+      ]
       y = 0.8
       0.upto(2) do |i|
         page_header_data_box headings[i], 4, y, 1.5, 0.2, :left
@@ -68,13 +106,15 @@ class SO < VarlandPdf
                 align: :center,
                 valign: :center
     end
+
+    #Draw shop order notes
     bounding_box [_i(0.25), _i(2.25)], width: _i(8), height: _i(2) do
       stroke_bounds
-      font_size 11.96
+      font_size 11.5
       fill_color '000000'
-      font 'Source Code Pro', style: :bold
+      font 'SF Mono', style: :bold
         text_box  @data['note'].join("\n"),
-                  at: [_i(0.05), _i(1.9)],
+                  at: [_i(0.05), _i(1.95)],
                   width: _i(7.9),
                   height: _i(2.5),
                   align: :left,
@@ -106,6 +146,16 @@ class SO < VarlandPdf
       # Draw shaded header boxes.
       fill_color 'cccccc'
       fill_rectangle([_i(3.2), _i(4)], _i(4.8), _i(0.7))
+      fill_rectangle([_i(3.2), _i(5)], _i(4.8), _i(0.2)) 
+      fill_rectangle([_i(6.5), _i(5)], _i(1.5), _i(0.2))
+      fill_rectangle([_i(0), _i(0.8)], _i(3.2), _i(0.2))
+
+      #Stroke lines for SHIP TO, FINAL INSPECTION, and P&M DESC.
+      stroke_color '000000'
+      stroke_line [_i(3.2), _i(4.8)], [_i(8.0), _i(4.8)]
+      #stroke_line [_i(4.0), _i(4.8)], [_i(4.0), _i(5.0)]
+      #stroke_line [_i(6.5), _i(4.8)], [_i(8.0), _i(4.8)]
+      stroke_line [_i(0), _i(0.6)], [_i(3.2), _i(0.6)]
 
       # Draw border around entire box.
       stroke_color '000000'
@@ -137,22 +187,27 @@ class SO < VarlandPdf
       page_header_text_box 'By', 6.2, 3.7, 0.45, 0.4
       page_header_text_box '#', 6.65, 3.7, 0.45, 0.4
       page_header_text_box 'Thickness', 7.1, 3.7, 0.9, 0.4
-      page_header_text_box 'Ship To:', 3.25, 5, 3.2, 0.2, false, :left
-      page_header_text_box 'Final Inspection By:', 6.5, 5, 1.5, 0.2, false, :center
-      page_header_text_box 'Part & Material Description:', 0.05, 0.8, 3.9, 0.2, false, :left
+      page_header_text_box 'Ship To', 3.2, 5, 3.5, 0.2, false, :center
+      page_header_text_box 'Final Inspection', 6.5, 5, 1.5, 0.2, false, :center
+      page_header_text_box 'Part & Material Description', 0, 0.8, 3.2, 0.2, false, :center
 
       # Draw header data.
-      
-      page_header_data_box @data['certify_code']['part_1'].join("\n") + "\n" + @data['process_specification'].join("\n") + "\n" + @data['certify_code']['part_2'].join("\n") , 0, 4.95, 4, 3.5, :left, false, :top
+      shipping_no = @data['shipping_phone'].to_i
+      ship_to_address = @data['ship_to']['name_1'] + "\n" + @data['ship_to']['address'] + "\n" + @data['ship_to']['city'] + ', ' + @data['ship_to']['state'] + ', ' + @data['ship_to']['zip_code'].to_s
+
+      page_header_data_box number_to_phone(shipping_no, area_code: true), 3.2, 4.8, 3.3, 0.2, :right
+      page_header_data_box ship_to_address, 3.2, 4.9, 3.2, 0.8, :left
+      page_header_data_box @data['certify_code']['part_1'].join("\n") + "\n" + @data['process_specification'].join("\n") + "\n" + @data['certify_code']['part_2'].join("\n"), 0.025, 4.95, 4, 3.5, :left, false, :top
       page_header_data_box @data['part_description'].join("\n"), 0, 0.6, 4, 0.6, :left
-      page_header_data_box (number_with_precision(@data['pieces_per_pound'], precision: 3)) + ' PCS/LB', 3.2, 0.8, 2.4, 0.2, :left
-      page_header_data_box (number_with_precision(@data['square_feet_per_pound'], precision: 2)) + ' FT<sup>2</sup>/LB', 3.2, 0.6, 2.4, 0.2, :left
-      page_header_data_box (number_with_precision(@data['pounds_per_cubic_foot'], precision: 2)) + ' LBS/FT<sup>3</sup>', 3.2, 0.4, 2.4, 0.2, :left
-      page_header_data_box (number_with_precision(@data['grams_per_piece'], precision: 6)) + ' GRAMS/PC', 3.2, 0.2, 2.4, 0.2, :left
-      page_header_data_box (number_with_precision(@data['pounds_per_thousand'], precision: 2)) + ' LBS/M', 5.6, 0.8, 2.4, 0.2, :left
-      page_header_data_box (number_with_precision(@data['square_feet_per_thousand'], precision: 2)) + ' FT<sup>2</sup>/M', 5.6, 0.6, 2.4, 0.2, :left
+      page_header_data_box (number_with_precision(@data['pieces_per_pound'], precision: 3)) + ' PCS / LB', 3.2, 0.8, 2.4, 0.2, :left
+      page_header_data_box (number_with_precision(@data['square_feet_per_pound'], precision: 2)) + " FT² / LB", 3.2, 0.6, 2.4, 0.2, :left
+      page_header_data_box (number_with_precision(@data['pounds_per_cubic_foot'], precision: 2)) + ' LB / FT³', 3.2, 0.4, 2.4, 0.2, :left
+      page_header_data_box (number_with_precision(@data['grams_per_piece'], precision: 6)) + ' GRAMS / PC', 3.2, 0.2, 2.4, 0.2, :left
+      page_header_data_box (number_with_precision(@data['pounds_per_thousand'], precision: 2)) + ' LBS / M', 5.6, 0.8, 2.4, 0.2, :left
+      page_header_data_box (number_with_precision(@data['square_feet_per_thousand'], precision: 2)) + 'FT² / M', 5.6, 0.6, 2.4, 0.2, :left 
       page_header_data_box (number_with_precision(@data['piece_weight'], precision: 5)) + ' PC WT', 5.6, 0.4, 2.4, 0.2, :left
-      page_header_data_box (number_with_precision(@data['square_centimeters_per_piece'], precision: 6)) + ' CM<sup>2</sup>/PC', 5.6, 0.2, 2.4, 0.2, :left
+      page_header_data_box (number_with_precision(@data['square_centimeters_per_piece'], precision: 6)) + 'CM² / PC', 5.6, 0.2, 2.4, 0.2, :left 
+      
       font 'Arial', style: :bold
       font_size 40
       fill_color 'ff0000'
@@ -199,8 +254,10 @@ class SO < VarlandPdf
         barcode.annotate_pdf self, height: _i(0.3)
       end
 
+      #Draw text under barcode
       bounding_box [_i(0.25), _i(10.45)], width: _i(7.5), height: _i(0.2) do
-        page_header_data_box '04/06/18 22:48', 0, 0.2, 7.55, 0.2, :right
+        page_header_data_box 'VMS' + @data['shop_order'].to_s, 5.5, 0.2, 7.55, 0.2, :left
+        page_header_data_box @data['shop_order_date*'] + ' ' +  @data['time_received'], 0, 0.2, 7.55, 0.2, :right
       end
       
       # Draw page header box.
@@ -269,62 +326,85 @@ class SO < VarlandPdf
         page_header_data_box @data['process_code'], 4.5, 1.6, 0.7, 0.3, :center, true
         page_header_data_box @data['part_id'], 5.2, 1.6, 2, 0.3, :left, true
         page_header_data_box @data['sub_id'], 7.2, 1.6, 0.3, 0.3, :center, true
-        page_header_data_box @data['equipment_used'].join("\n"), 0, 1.025, 3.5, 0.6, :left, false, :top
+        page_header_data_box @data['equipment_used'].join("\n"), 0, 1.025, 3.5, 0.6, :left, true, :top
         page_header_data_box @data['part_name'].join("\n"), 3.5, 1.025, 2.5, 0.6, :left, false, :top
-        page_header_data_box @data['po_numbers'].join("\n"), 6, 1.025, 1.5, 0.6, :left, false, :top
+        page_header_data_box @data['po_numbers'].join("\n"), 6, 1.025, 1.5, 0.6, :left, true, :top
         page_header_data_box @data['shop_order_date*'], 0, 0.3, 1.5, 0.3, :center, true
         page_header_data_box (number_with_precision(@data['pounds'], precision: 2)).to_s, 1.5, 0.3, 1.25, 0.3, :center, true
         page_header_data_box (number_with_delimiter(@data['pieces'])).to_s, 2.75, 0.3, 1.25, 0.3, :center, true
         page_header_data_box @data['containers'].to_s + " " +  @data['container_type'], 4, 0.3, 2, 0.3, :center, true
-        page_header_data_box '$/M', 6, 0.3, 1.5, 0.3, :right, false, :bottom 
-
-        puts (@data['po_numbers'].join("\n")).gsub("", " ")
-
+        page_header_data_box '$/' + @data['price_per'], 6, 0.3, 1.5, 0.3, :right, true, :bottom 
       end
 
     end
 
   end
 
-  def page_header_text_box(text, x, y, width, height = 0.2, large = false, align = :center, valign = :center)
-    font 'Arial Narrow', style: :bold
-    font_size large ? 14 : 9
-    fill_color '000000'
-    text_box  text.upcase,
-              at: [_i(x), _i(y)],
-              width: _i(width),
-              height: _i(height),
-              align: align,
-              valign: valign,
-              overflow: :shrink_to_fit
-  end
-
-  def page_header_data_box(text, x, y, width, height = 0.3, align = :left, large = false, valign = :center)
-    return if text.blank?
-    font 'Arial Narrow', style: :bold
-    font_size large ? 16 : 11
-    fill_color '000000'
-    case @color
-    when 'red'
-      fill_color 'ffffff'
-    end
-    x += 0.05
-    width -= 0.1
-    text_box  text,
-              at: [_i(x), _i(y)],
-              width: _i(width),
-              height: _i(height),
-              align: align,
-              valign: valign,
-              inline_format: true
-  end
-
-  def print_text
+  def print_instructions
 
     start_new_page
     font_size 11.5
     fill_color '000000'
-    font 'SF Mono', style: :bold
+    font 'Source Code Pro', style: :bold
+
+    #Begin printing
+    bounding_box([_i(0.25), _i(8.2)], :width => _i(8.0), :height => _i(7.75)) do
+      opCode_length = @data['operations'].length - 1
+      specInstr_length =  @data['special_instructions'].length - 1
+
+      puts "Length of opCode is: #{opCode_length+1}"
+      puts "Length of specInstr is: #{specInstr_length+1}"
+
+
+      #Print Instructions
+      for i in 0..opCode_length do
+        text @data['operations'][i]['code'] + ".  " + @data['operations'][i]['title'] 
+        
+        for j in 0..specInstr_length do
+
+          if@data['special_instructions'][j]['operation'] == @data['operations'][i]['code']
+
+            if @data['special_instructions'][j]['before_after'].eql? "before"
+              @data['special_instructions'][j]['text'].each do |l|
+
+                if l.blank?
+                  text "\xC2\xA0"
+                else 
+                  text l.gsub(/^([^\S\r\n]+)/m) { |m| "\xC2\xA0" * m.size }
+                end
+
+              end
+
+            end
+
+          end
+          
+        end
+
+        if ((@data['operations'][i].length > 7) && (@data['operations'][i]['code_details']['text'] != nil))
+
+          @data['operations'][i]['code_details']['text'].each do |l|
+            if l.blank?
+              text "\xC2\xA0"
+            else 
+              text l.gsub(/^([^\S\r\n]+)/m) { |m| "\xC2\xA0" * m.size }
+            end
+            
+          end
+
+        end
+        
+        text "\n"
+      end
+
+    end
+
+  end
+
+
+  def testShopOrderNotes
+    start_new_page
+    #TEST shop order (format & fonts)
     bounding_box [_i(0.25), _i(8)], width: _i(8), height: _i(7.75) do
       2.times do
         text ('=' * 31) + ' SHOP ORDER NOTES ' + ('=' * 31)
@@ -343,7 +423,39 @@ class SO < VarlandPdf
         text ' '
       end
     end
+  end
 
+  def page_header_text_box(text, x, y, width, height = 0.2, large = false, align = :center, valign = :center)
+    font 'Arial Narrow', style: :bold
+    font_size large ? 14 : 9
+    fill_color '000000'
+    text_box  text.upcase,
+              at: [_i(x), _i(y)],
+              width: _i(width),
+              height: _i(height),
+              align: align,
+              valign: valign,
+              overflow: :shrink_to_fit
+  end
+
+  def page_header_data_box(text, x, y, width, height = 0.3, align = :left, large = false, valign = :center)
+    return if text.blank?
+    font 'Arial Narrow', style: :bold
+    font_size large ? 14 : 11
+    fill_color '000000'
+    case @color
+    when 'red'
+      fill_color 'ffffff'
+    end
+    x += 0.05
+    width -= 0.1
+    text_box  text,
+              at: [_i(x), _i(y)],
+              width: _i(width),
+              height: _i(height),
+              align: align,
+              valign: valign,
+              inline_format: true
   end
 
 end
